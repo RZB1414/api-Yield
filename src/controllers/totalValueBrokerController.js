36dba58754dbb84f6b096712c55f4258d70da1cc
Renan Buiatti <renanbuiatti14@gmail.com>
2025-05-08 22:10:40 +0700

    adding more stuff

--- src/controllers/totalValueBrokerController.js
+++ src/controllers/totalValueBrokerController.js
@@ -47,6 +47,38 @@ class TotalValueBrokerController {
         }
     }
 
+    static async updateTotalValueBroker(req, res) {
+        const { id } = req.params
+        if (!id) {
+            return res.status(400).send('ID is required')
+        }
+        try {
+            const updatedTotalValueBroker = await totalValueBroker.findByIdAndUpdate(id, req.body, { new: true })
+            if (!updatedTotalValueBroker) {
+                return res.status(404).send('Total value broker not found')
+            }
+            res.status(200).json({ message: 'Total value broker updated', data: updatedTotalValueBroker })
+        } catch (error) {
+            res.status(500).json({ msg: "Error updating total value broker", error: error.message })
+        }
+    }
+    
+    static async deleteTotalValueBroker(req, res) {
+        const { id } = req.params
+        if (!id) {
+            return res.status(400).send('ID is required')
+        }
+        try {
+            const deletedTotalValueBroker = await totalValueBroker.findByIdAndDelete(id)
+            if (!deletedTotalValueBroker) {
+                return res.status(404).send('Total value broker not found')
+            }
+            res.status(200).json({ message: 'Total value broker deleted', data: deletedTotalValueBroker })
+        } catch (error) {
+            res.status(500).json({ msg: "Error deleting total value broker", error: error.message })
+        }
+    }
+
 }
 
 export default TotalValueBrokerController
--- src/routes/totalValueBrokersRoutes.js
+++ src/routes/totalValueBrokersRoutes.js
@@ -6,5 +6,7 @@ const routes = express.Router()
 routes.post('/auth/createTotalValueBroker', TotalValueBrokerController.createTotalValueBroker)
 routes.get('/auth/getAllTotalValueBrokers', TotalValueBrokerController.getAllTotalValueBrokers)
 routes.get('/auth/getTotalValueBroker/:id', TotalValueBrokerController.getTotalValueBrokerById)
+routes.put('/auth/updateTotalValueBroker/:id', TotalValueBrokerController.updateTotalValueBroker)
+routes.delete('/auth/deleteTotalValueBroker/:id', TotalValueBrokerController.deleteTotalValueBroker)
 
 export default routes
